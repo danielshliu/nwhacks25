2bcc4e4bba900fade58a410d7f528cde0c8fd52b
Teaapoo <dliu224@my.yorku.ca>
2025-01-19 05:45:18 -0800

    page...

--- calorific/src/app/login/page.js
+++ calorific/src/app/login/page.js
@@ -47,17 +47,17 @@ export default function login() {
         <h2 className="text-2xl font-bold mb-2 text-center text-black">
           Log in
         </h2>
-        <p className="text-center mb-8 text-black">
+        <p className="text-center mb-4 text-black">
           Don't have an account?{" "}
-          <a href="/signup" className="underline">
+          {/* <a href="/signup" className="underline">
             Sign up
-          </a>
+          </a> */}
         </p>
 
         <SignedOut>
           <SignInButton>
             <button className="w-full bg-[#E4B7A0] text-black py-3 rounded-full hover:bg-[#d6a08c] focus:outline-none transition-colors">
-              Sign In
+              Sign In / Sign up
             </button>
           </SignInButton>
         </SignedOut>
@@ -65,40 +65,7 @@ export default function login() {
           <UserButton />
         </SignedIn>
 
-        {/* <form action="#" method="POST">
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"
-              className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-gray-500 bg-transparent text-black"
-              required
-            />
-          </div>
-          <div className="mb-8">
-            <label htmlFor="password" className="block text-gray-700 mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-gray-500 bg-transparent text-black"
-              placeholder="Password"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-[#E4B7A0] text-black py-3 rounded-full hover:bg-[#d6a08c] focus:outline-none transition-colors"
-          >
-            Log in
-          </button>
-        </form> */}
+
       </div>
     </div>
   );
